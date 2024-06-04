fc1a2d0f022f8c193e9d4a34e3439447966e717c
Mach3tryhard <matei.sirghe@gmail.com>
2024-06-04 21:29:41 +0300

    fixed some stuff

---
+++ public/script/script.js
@@ -0,0 +1,23 @@
+var rez=0;
+const test = document.getElementById("opennavbutton");
+const test1 = document.getElementById("mySidenav");
+
+function closebar()
+{
+    console.log(rez);
+    rez+=1;
+    if(rez==2)
+    {
+        closeNav()
+        rez=0;
+    }
+}
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "267px";
+}
+
+function closeNav() {
+    rez=0;
+    document.getElementById("mySidenav").style.width = "0px";
+}
