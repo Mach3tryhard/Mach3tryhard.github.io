9d535e36ad5a883d88b2e30a71879fee19d92195
Mach3tryhard <matei.sirghe@gmail.com>
2024-06-07 00:32:29 +0300

    made the fancy fucking cursor

--- public/script/cursor.js
+++ public/script/cursor.js
@@ -3,9 +3,20 @@ const cursorPointed = document.getElementById("cursor");
 const moveCursor = (e)=> {
   const mouseY = e.clientY;
   const mouseX = e.clientX;
-  
-  cursorPointed.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
- 
+  //console.log(mouseX);
+  //console.log(mouseY);
+  cursorPointed.style.transform = `translate3d(${mouseX-22}px, ${mouseY-22}px, 0) rotate(45deg)`;
+  cursorPointed.style.backgroundPosition = mouseX%1000+500+"px "+mouseY%1000+"px";
+
 }
 
 window.addEventListener('mousemove', moveCursor)
+
+function hide()
+{
+  cursorPointed.style.visibility = "hidden";
+}
+function unhide()
+{
+  cursorPointed.style.visibility = "visible";
+}
