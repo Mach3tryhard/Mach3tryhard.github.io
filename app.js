e6fb2b7272afd755d7a0d05787e8d66e013699d5
Mach3tryhard <matei.sirghe@gmail.com>
2024-06-09 01:52:26 +0300

    test for new page

--- app.js
+++ app.js
@@ -18,4 +18,8 @@ app.use((req, res, next) => {
     res.status(404).sendFile(path.join(__dirname,'views','404.html'));
 });
 
+app.get('/achievements', function (req, res) {
+    res.sendfile(homeRoute + '/achievements.html');
+});
+
 app.listen(3000);
